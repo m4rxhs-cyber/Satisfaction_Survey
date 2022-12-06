b62307bc0754176be60e8cd88002e7e70596e7f3
Neemias <neemias.sales@technolog.com.br>
2022-12-06 17:15:55 -0300

    arrumando caminho de pastas e links de redirecionamento

--- Views/scripts/comunicaApiLogar.js
+++ Views/scripts/comunicaApiLogar.js
@@ -9,8 +9,8 @@ function logar() {
             for (i = 0; i < resposta.length; i++) {
                 if (resposta[i].email == login && resposta[i].senha == senha) {
                     if (resposta[i].grupo == 'aluno') {
-                        window.location.href = "./view_cadastrar_avaliacao.html";
-                    } else { window.location.href = "./view_avaliacoes.html" }
+                        window.location.href = "./Satisfaction_Survey/Views/view_cadastrar_avaliacao.html";
+                    } else { window.location.href = "./Satisfaction_Survey/Views/view_avaliacoes.html" }
                 } else {
                     Swal.fire({
                         icon: 'error',
